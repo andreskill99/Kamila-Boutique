fe7c8037e71d676f3872660b83c9dfe79287b2f5
Andres <andresgallegos781@gmail.com>
2021-04-08 18:44:06 -0500

    Initial commit

---
+++ app/src/main/java/com/classroom/app1/UI/ClickListeners/RecyclerViewClickListenerOrder.java
@@ -0,0 +1,12 @@
+package com.classroom.app1.UI.ClickListeners;
+
+import android.view.View;
+
+import com.classroom.app1.Model.Order;
+
+
+
+
+public interface RecyclerViewClickListenerOrder {
+    abstract void onClick(View view, Order product);
+}
---
+++ app/src/main/java/com/classroom/app1/UI/ClickListeners/RecyclerViewClickListenerProduct.java
@@ -0,0 +1,12 @@
+package com.classroom.app1.UI.ClickListeners;
+
+import android.view.View;
+
+import com.classroom.app1.Model.Product;
+
+
+
+
+public interface RecyclerViewClickListenerProduct {
+     abstract void onClick(View view, Product product);
+}
